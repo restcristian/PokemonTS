ea2d55bb42d5d3f84ab7623d07be6b0119f4c0bd
Cristian Restituyo <cristian.restituyo@denkwerk.com>
2020-09-26 22:58:11 +0200

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import { Route, Switch } from "react-router-dom";
 import routes from "./routes";
 import "./App.css";
 
-const App: React.FC = () => (
-  <div className="App">
-    <Switch>
-      {routes.map(route => (
-        <Route path={route.path} component={route.component} />
-      ))}
-    </Switch>
-  </div>
-);
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Switch>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            component={route.component}
+          />
+        ))}
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
